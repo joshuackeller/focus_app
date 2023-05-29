e4fc0542003a12d2966634e4b3ca496502203e52
joshuackeller <joshuackeller@gmail.com>
2023-05-28 22:17:52 -0600

    deploy with correct secret

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const { secret } = await req.json();
-  if (secret !== "guineo") throw Error("Incorrect");
+  if (secret !== process.env.SECRET) throw Error("Incorrect");
 
   if (process.env.JWT_SECRET) {
     const token = jwt.sign({}, process.env.JWT_SECRET);
